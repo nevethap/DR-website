a9c90cb13e41e09bbb241f22ba3793ec7a0aa695
Aashish Pundalik <pundalikaashish@gmail.com>
2017-02-26 13:35:20 +0530

    |Aashish| Added hardcoded values for precision and recall in the predictions API |

--- apis/utils/prediction_transformation_util.py
+++ apis/utils/prediction_transformation_util.py
@@ -2,11 +2,14 @@ import numpy as np
 
 class PredictionTransformationUtil:
     def transform(self, test_labels, predictions, files):
-        transformed_predictions = []
+        predictions_all = {}
+        predictions_all['predictions'] = []
         for label, predicted_res, image_name in zip(test_labels, predictions, files):
             prediction_dict = {}
             prediction_dict['actual'] = np.asscalar(label)
             prediction_dict['predicted'] = np.asscalar(predicted_res[0])
             prediction_dict['img_url'] = image_name
-            transformed_predictions.append(prediction_dict)
-        return transformed_predictions
+            predictions_all['predictions'].append(prediction_dict)
+        predictions_all['precision'] = 90 # TODO: remove hardcoding with actual values
+        predictions_all['recall'] = 90 # TODO: remove hardcoding with actual values
+        return predictions_all
